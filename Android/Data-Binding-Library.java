77554f5ac5072fc59d2751978201679234bbcdd4
kai <1058495760@qq.com>
2017-06-30 21:52:59 +0800

    binding data 的学习

--- Android/Data-Binding-Library.java
+++ Android/Data-Binding-Library.java
@@ -53,3 +53,112 @@ Binding Data
 			binding.setUser(user);
 		}
 	}
+
+
+Import
+
+	import just like java :
+
+	<data>
+		<import type="android.view.View"/>
+	</data>
+	<TextView
+		android:text="@{user.lastName}"
+		android:visibility="@{user.isAdult ? View.VISIBLE : View.GONE}"
+	></TextView>
+
+	Or like this
+
+	<data>
+	    <import type="com.example.MyStringUtils"/>
+	    <variable name="user" type="com.example.User"/>
+	</data>
+…
+	<TextView
+		android:text="@{MyStringUtils.capitalize(user.lastName)}"
+		android:layout_width="wrap_content"
+		android:layout_height="wrap_content"/>
+
+Variables
+
+	<data>
+	    <import type="android.graphics.drawable.Drawable"/>
+	    <variable name="user"  type="com.example.User"/>
+	    <variable name="image" type="Drawable"/>
+	    <variable name="note"  type="String"/>
+	</data>
+
+Custom Binding Class Names
+
+	default ---> My moduule package + databinding + layoutNameBinding
+		eg. com.sample.myapp.databinding.MainActivityBinding
+
+	custom 
+			---><data class="ContactItem">
+				    ...
+				</data>
+
+				<data class=".ContactItem">
+				    ...
+				</data>
+
+				<data class="com.example.ContactItem">
+				    ...
+				</data>
+
+&lt;  &gt; --> <>
+
+
+Data Objects
+	
+	POJO may be used for data binding ,but will not cause the UI updata;
+
+	Use
+		---> Observable objects, observable fields, and observable collections.
+
+		Observable Objects
+
+			-->
+				private static class User extends BaseObservable {
+				   private String firstName;
+				   private String lastName;
+				   @Bindable
+				   public String getFirstName() {
+				       return this.firstName;
+				   }
+				   @Bindable
+				   public String getLastName() {
+				       return this.lastName;
+				   }
+				   public void setFirstName(String firstName) {
+				       this.firstName = firstName;
+				       notifyPropertyChanged(BR.firstName);
+				   }
+				   public void setLastName(String lastName) {
+				       this.lastName = lastName;
+				       notifyPropertyChanged(BR.lastName);
+				   }
+				}
+
+				BR generated 
+
+		Observable Fields
+
+			-->
+				private static class User {
+				   public final ObservableField<String> firstName =
+				       new ObservableField<>();
+				   public final ObservableField<String> lastName =
+				       new ObservableField<>();
+				   public final ObservableInt age = new ObservableInt();
+				}
+
+		Observable Collections
+
+			-->
+				ObservableArrayMap<String, Object> user = new ObservableArrayMap<>();
+				user.put("firstName", "Google");
+				user.put("lastName", "Inc.");
+				user.put("age", 17);
+
+
