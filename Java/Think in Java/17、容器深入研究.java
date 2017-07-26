56deb011c788ca1caae732d33624c66ce266287a
kai <1058495760@qq.com>
2017-07-27 07:56:06 +0800

    添加容器深入理解一章的笔记，Map笔记

---
+++ Java/Think in Java/17、容器深入研究.java
@@ -0,0 +1,29 @@
+
+
+7、队列
+	
+	优先级队列
+
+		优先级队列的排序顺序是通过 Comparable 进行控制的
+
+
+8、理解Map
+
+	HashMap
+
+	TreeMap
+
+	LinkedHashMap
+
+	WeakHashMap
+
+	ConcurrentHashMap
+
+	IdentifyHashMap
+
+	
+	Map 的键要求和 Set 的一样，任何键都必须具有一个 equals()方法；
+
+	如果键用于散列 Map 还必须具有恰当的 hashCod()方法
+
+	如果用于 TreeMap 还必须实现 Comparable 接口
