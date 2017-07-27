2c9071c130d42d44e54809dc2bcce4fcdc32a2b1
kai <1058495760@qq.com>
2017-07-27 22:32:37 +0800

    添加散列码的笔记 Hashcode

--- Java/Think in Java/17、容器深入研究.java
+++ Java/Think in Java/17、容器深入研究.java
@@ -40,3 +40,19 @@
 		return o instanceof ThisClass && 
 			(this.number == ((ThisClass)o).number);
 	}
+
+9、散列和散列码 hashcode
+
+	在一个运行的进程中，相等的对象必须要有相同的哈希码
+
+	不相等的对象可能也有相同的哈希码
+
+	有同一个哈希值的对象不一定相等
+
+	通过重写 equals 方法，你将申明一些对象与其他对象相等，
+	但是原始的 hashCode 方法将所有的对象看做是不同的。所
+	以你将会有不同哈希码的相同对象。例如，在 HashMap 中
+	调用 contains 方法将会返回 false，即使这个对象已经
+	被添加。
+
+	
