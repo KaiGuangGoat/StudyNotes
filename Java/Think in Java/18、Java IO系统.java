0117dba79970abdb6028494f25632be6e49721f1
kai <1058495760@qq.com>
2017-08-02 22:06:45 +0800

    添加NIO的笔记

--- Java/Think in Java/18、Java IO系统.java
+++ Java/Think in Java/18、Java IO系统.java
@@ -102,11 +102,52 @@
 	System.out ---> PrintStream
 	System.in  ---> InputStream
 
-NIO
+
+
+5、NIO
 
 NIO 有三大核心部分：Channel,Buffer,Selector。数据总是从通道读取到缓冲区，
 或者是从缓冲区写入通道
 
 唯一与通道交互的缓冲器是 ByteBuffer
 
-FileChannel:用于读取、写入、映射和操作文件的通道
+	Channel:interface
+	+close():-->关闭通道
+	+isOpen():return boolean-->判断通道是否打开
+		||
+		||
+		ReadableByteChannel:interface
+		+read(ByteBuffer dst):return int-->将字节序列从通道读取到指定的缓冲区
+		WritableByteChannel:interface
+		+write(ByteBuffer src):return int-->将字节序列从给定的缓冲区写入此通道
+		ByteChannel extends ReadableByteChannel,WritableByteChannel:interface
+			DatagramChannel
+			FileChannel
+			SocketChannel
+
+	FileChannel:用于读取、写入、映射和操作文件的通道
+
+	Buffer
+		Buffer 的使用步骤：
+			1、分配空间
+			2、写入数据到Buffer
+			3、调用 flip()方法
+			4、从 Buffer 中读取数据
+			5、调用clear()或compact()方法
+
+			写数据到 Buffer：
+				1、Channel 向 Buffer 写数据:fileChannel.read(buf);
+				2、通过 Buffer 的put():buf.put(...);
+			从 Buffer 读数据：
+				1、从 Buffer 读取到Channel:channel.write(buf);
+				2、通过 Buffer 的get()方法:buf.get();
+
+		Buffer 的几个重要变量：
+
+			capacity		缓冲区的总长度
+
+			position		下一个要操作的数据元素的位置
+
+			limit 			缓冲区数组中不可操作的下一个元素的位置：limit<=capacity
+
+			mark			用于标记当前position的前一个位置
